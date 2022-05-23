f33a953a02e068a5aa1be067a3f02418c3192e25
Yulia Pakhachuk <yulia.pakhachuk@gmail.com>
2022-05-23 22:54:56 +0200

    fix buggs in header-style script

--- index.js
+++ index.js
@@ -2,21 +2,49 @@
 // нашему мобільному меню, яке у нас закрите.
 
 
+const header = document.querySelector(".header");
+const headerBtn = document.querySelector(".header__btn");
+const logoIcon = document.querySelector(".header__logo use");
 const openBtn = document.querySelector(".open__menu");
-console.log(openBtn);
-// openBtn.onclick = () => { closeMobileMenu() };
-// openBtn.onclick = changeMenuState; // метод події онклік ТІЛЬКИ ОДНА ПОДІЯ
+const mobileMenu = document.querySelector(".mobile__menu");
+
 
 openBtn.addEventListener("click", changeMenuState);
-// openBtn.addEventListener("click", () => { 
-//     mobileMenu.classList.toggle("is-open");
-// });
-const mobileMenu = document.querySelector(".mobile__menu");
-console.log(mobileMenu);
+
 function changeMenuState(event) { 
     mobileMenu.classList.toggle("is-open");
-    // console.log(event);
+
+    if (mobileMenu.classList.contains('is-open')) {
+        header.classList.add("scroled");
+        logoIcon.setAttribute("href", "./images/icons.svg#logo");
+        openBtn.firstElementChild.setAttribute("href", "./images/icons.svg#close");
+    } else { 
+        openBtn.firstElementChild.setAttribute("href", "./images/icons.svg#menu");
+    }
 }
+
+window.addEventListener("scroll", changeHeaderStyles);
+
+function changeHeaderStyles() { 
+
+    if (window.scrollY > 0) {
+        header.classList.add("scroled");
+        logoIcon.setAttribute("href", "./images/icons.svg#logo");
+    } else { 
+        if (mobileMenu.classList.contains('is-open')) { 
+            return;
+        }
+        header.classList.remove("scroled");
+        logoIcon.setAttribute("href", "./images/icons.svg#logo-colored");
+    }
+
+    // (window.scrollY > 0) ?
+    //     header.classList.add("scroled") : header.classList.remove("scroled");
+    // window.scrollY > 0 ?
+    //     logoIcon.setAttribute("href", "./images/icons.svg#logo") :
+    //     logoIcon.setAttribute("href", "./images/icons.svg#logo-colored");
+}
+
 
 // const menuLink = document.querySelector(".mob__menu--link");
 // document.addEventListener("mousemove", (event) => { 
@@ -26,34 +54,5 @@ function changeMenuState(event) {
 //     const calcX = screenX < middleX ? -screenX : screenX;
 //     const calcY = screenY < middleY ? -screenY : screenY;
 //     menuLink.style.transform = `translate(${calcX / 50}px, ${calcY / 50}px)`;
-
 // })
-
-
-// слухаи подію скрола на документі
-//     коли значення позиції скрола по У буде більше нуля,
-//     то додавати клас scroled
-// якщо позиція дорівнює нулю, то прибирати цей клас
-
-// вивчити обєкт події для скрола і
-// подивитись чи можна взяти позицію скрола звідти ?
-
-const header = document.querySelector(".header");
-const headerBtn = document.querySelector(".header__btn");
-const logoIcon = document.querySelector(".header__logo use");
-console.log(logoIcon);
-
-window.addEventListener("scroll", changeHeaderStyles);
-function changeHeaderStyles() { 
-    window.scrollY > 0 ?
-        header.classList.add("scroled") : header.classList.remove("scroled");
-    window.scrollY > 0 ?
-        headerBtn.classList.add("scroled") : headerBtn.classList.remove("scroled");
-    window.scrollY > 0 ?
-        logoIcon.setAttribute("href", "./images/icons.svg#logo") :
-        logoIcon.setAttribute("href", "./images/icons.svg#logo-colored");
-
-}
-
-
 
